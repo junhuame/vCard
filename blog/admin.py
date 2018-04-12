93a32d504f9c522c3354546efd4177d3bd12cc10
wangjunhua <email@junh.wang>
2018-04-12 21:22:12 +0800

    博客后台管理

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from .models import Category, Post, Comment
 
-# Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    """日志分类管理"""
+    list_display = ('name', 'slug', 'description')
+
+
+class PostAdmin(admin.ModelAdmin):
+    """日志管理"""
+    list_display = ('title', 'slug', 'Category', 'status', 'publish')
+    list_editable = ('status',)
+    search_fields = ('title', 'body')
+    date_hierarchy = 'publish'
+    list_filter = ('Category', 'status', 'publish')
+    list_per_page = 20
+
+
+class CommentAdmin(admin.ModelAdmin):
+    """日志评论管理"""
+    list_display = ('name', 'post', 'email', 'active', 'created')
+    list_per_page = 20
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Post, PostAdmin)
+admin.site.register(Comment, CommentAdmin)
+
+
