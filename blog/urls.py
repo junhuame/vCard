c17b7ef0eec409eada73218fea8c1b3ed3184923
wangjunhua <email@junh.wang>
2018-04-13 18:15:49 +0800

    博客详情页

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', view=views.blog_list, name='blog_list'),
+    url(r'^(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<day>\d{1,2})/(?P<slug>[-\w]+)/$', view= views.blog_datail, name='blog_datail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Category, Post, Comment
 
@@ -7,3 +7,16 @@ def blog_list(request):
     """博客首页"""
     posts = Post.objects.all()
     return render(request, 'blog-list.html', {'posts':posts})
+
+
+def blog_datail(request, year, month, day, slug):
+    """博客日志详情页"""
+    post = get_object_or_404(
+        Post, 
+        slug = slug,
+        publish__year = year,
+        publish__month = month,
+        publish__day = day,
+        status = 'published'
+        )
+    return render(request, 'blog-datail.html', {'post':post})
